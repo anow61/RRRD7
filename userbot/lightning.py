db8a37f7368689388acce5d4ce93864cbf74a112
JMTHON <83249685+JMTHON@users.noreply.github.com>
2021-05-02 10:18:54 +0300

    Add files via upload

---
+++ userbot/lightning.py
@@ -0,0 +1,22 @@
+#kang krne aaye ho  chala ja bhosdike tere kaam ka ni h ye
+#make by legendx22 for chutiapa 🤔😑 you dont kang this 
+#okay to ab isme cheeze bharte h 
+#if you read only this then okay else
+#chala jaa bhosdike 
+import os
+from userbot import ALIVE_NAME 
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "LIGHTNING"
+NAME = os.environ.get("BOT_NICK_NAME")
+BOT =  = str(NAME) if NAME else "BLACK LIGHTNING"
+#op for keinshin
+
+Keinshin = "[MUHAMMAD](https://t.me/RRRD7)"
+OP = "[JMTHON TEAM](https://github.com/JMTHON/RRRD7)"
+OKAY = "[SUPPORT GROUP](https://t.me/JMTHON)"
+#itna test h aur aage krte h
+#test successful raha ab aage 
+ALIVE = "JMTHON BOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22 for black lightning
+USERBOT = " HELLO MASTER MY NAME IS JMTHON BOT I AM A BEST USERBOT 💝"
+EMOJI = "⚡"
+#yrr isko apne bot me aply krne se pehle mere se pooch lena ok
+#aur aage add kruga abhi busy okay 🤔
