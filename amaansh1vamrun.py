31ff6dba9de3b5f05e69cb1d70e263fbd39a339d
JMTHON <83249685+JMTHON@users.noreply.github.com>
2021-05-02 09:41:03 +0300

    Add files via upload

---
+++ amaansh1vamrun.py
@@ -0,0 +1,5 @@
+#To solbe suspension issue #By JMTHON
+import os
+os.system("git clone https://github.com/JMTHON/RRRD7 /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+
+
